3f3a941f570f5164d42bf8dc32e1560e8c87a6a2
Thomas WACHE <thomas@wache.fr>
2018-05-15 16:41:35 +0200

    Updated examples from C API

--- examples/c_api.c
+++ examples/c_api.c
@@ -8,8 +8,12 @@
 #include "check.h"
 #include "exe_c_api.h"
 GENERATE_BINDINGS(rs_pe);
+GENERATE_BINDINGS(rs_elf32);
+GENERATE_BINDINGS(rs_elf64);
 
-#define safe_rs_pe_free(h)     SAFE_FREE(h, NULL, pe.ops->free_exe)
+const rs_parse_t parsers[] = { rs_pe_parse_helper, rs_elf32_parse_helper, rs_elf64_parse_helper, NULL };
+
+#define safe_rs_free(h)     SAFE_FREE(h.handle, NULL, h.ops->free_exe)
 
 char* show_flags(uint32_t flags) {
     static char str_flags[4] = "---";
@@ -24,7 +28,7 @@ int main(int argc, char *const argv[]) {
     void *ptr = MAP_FAILED;
     const uint8_t *data;
     struct stat sb;
-    rs_pe_handle_t pe = { NULL, NULL };
+    rs_object_t obj = { NULL, NULL };
     rs_handle_t section = NULL;
     size_t nsections;
     const char* section_name = NULL;
@@ -42,17 +46,27 @@ int main(int argc, char *const argv[]) {
     CHK_NEG(fstat(fd, &sb));
     CHK_MMAP(ptr = mmap(NULL, sb.st_size, PROT_READ, MAP_SHARED, fd, 0));
     data = (const uint8_t *)ptr;
-    CHK(rs_pe_parse_helper(&pe, data, sb.st_size), == false);
-    nsections = pe.ops->get_number_of_sections(pe.handle);
+
+    for ( size_t idx = 0; parsers[idx] != NULL; idx++ ) {
+        if ( parsers[idx](&obj, data, sb.st_size) ) {
+            break;
+        }
+    }
+    if ( obj.handle == NULL ) {
+        error("No parser match");
+        goto fail;
+    }
+
+    nsections = obj.ops->get_number_of_sections(obj.handle);
     for ( size_t idx = 0; idx < nsections; idx++ ) {
-        CHK_NULL(section = pe.ops->get_section_at(pe.handle, idx));
-        CHK_NULL(section_name = pe.ops->get_section_name_at(pe.handle, idx));
-        section_flags = pe.ops->get_flags(section);
-        section_size = pe.ops->get_size(section);
-        section_off = pe.ops->get_offset(section);
+        CHK_NULL(section = obj.ops->get_section_at(obj.handle, idx));
+        CHK_NULL(section_name = obj.ops->get_section_name_at(obj.handle, idx));
+        section_flags = obj.ops->get_flags(section);
+        section_size = obj.ops->get_size(section);
+        section_off = obj.ops->get_offset(section);
 
         printf(
-            "Section %02lu / %s : flags=%s, offset=0x%lx, size%lu\n",
+            "Section %02lu / %-20s : flags=%s, offset=0x%08lx, size=%lu\n",
             idx, section_name, show_flags(section_flags), section_off, section_size
         );
 
@@ -61,7 +75,7 @@ int main(int argc, char *const argv[]) {
     ret = EXIT_SUCCESS;
 
     fail:
-    safe_rs_pe_free(pe.handle);
+    safe_rs_free(obj);
     safe_munmap(ptr, sb.st_size);
     safe_close(fd);
     return ret;
